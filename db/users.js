6fbc4f436e970287236cff8b11a87db2088471ff
Christian Factoriza <cfactoriza@gmail.com>
2022-08-10 15:43:02 -0500

    Destroy/reactiveUser Functions, Get Users

--- db/users.js
+++ db/users.js
@@ -1,7 +1,10 @@
 const client = require("./client");
+const bcrypt = require('bcrypt');
 
 
 async function createUser({ email, password, firstName, lastName, isAdmin, isActive }){
+    const SALT_COUNT = 20;
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
 try {
     const{
         rows: [user],
@@ -11,13 +14,32 @@ try {
         ON CONFLICT (email) DO NOTHING
         RETURNING *
         `,
-        [email, password, firstName, lastName, isAdmin, isActive]
+        [email, hashedPassword, firstName, lastName, isAdmin, isActive]
     );
         return user
 } catch (error){
     throw error;
 }
 }
+
+async function getUser({email, password}){
+    try{
+        const user = await getUserByEmail(email);
+        const hashedPassword = user.password;
+        const passwordMatched = await bcrypt.compare(password, hashedPassword)
+        
+        if (passwordMatched){
+            delete user.password
+            return user
+        }
+    } catch (error){
+        throw error
+    }
+}
+
+
+
+
 async function getUserByEmail(email){
     try{ 
         const {
@@ -52,9 +74,55 @@ async function getUserById(userId){
     }
 }
 
+async function destroyUser(id){
+    try {
+        const {
+            rows:[user],
+        } = await client.query(
+            `
+            SELECT id, is_active
+            FROM users
+            WHERE id=$1
+            SET is_active = false
+            RETURNING *
+            `,
+            [id]
+        );
+        return user
+    } catch (error){
+        throw error
+    }
+}
+
+//An Admin function to reactive a user incase they want their account back
+async function reactivateUser(id){
+    try {
+        const {
+            rows:[user],
+        } = await client.query(
+            `
+            SELECT id, is_active
+            FROM users
+            WHERE id=$1
+            SET is_active = true
+            RETURNING *
+            `,
+            [id]
+        );
+        return user
+    } catch (error){
+        throw error
+    }
+}
+
+
+
 
 module.exports = {
     createUser,
+    getUser,
     getUserByEmail,
-    getUserById
+    getUserById,
+    destroyUser,
+    reactivateUser
 }
