10ca60695eec55c5ef10df4784cf47a147d4f0a3
rafaelbchs <rafaelbchacins@gmail.com>
2022-08-11 12:41:59 -0500

    attached photos and categories to products

--- db/products.js
+++ db/products.js
@@ -1,56 +1,60 @@
 const client = require("./client");
 
 // Adds a product to our db, this takes an object as parameter, returns a product object
-async function createProduct({name, description, price, quantity, isActive}){
-    try {
-        const {
-            rows: [product]
-        } = await client.query(
-            `
+async function createProduct({ name, description, price, quantity, isActive }) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
             INSERT into products(name, description, price, quantity_on_hand, is_active)
             VALUES($1, $2, $3, $4, $5)
             ON CONFLICT (name) DO NOTHING
             RETURNING *;
             `,
-            [name, description, price, quantity, isActive]
-        );
-        return product
-    } catch (error) {
-        console.error(error)
-    }
+      [name, description, price, quantity, isActive]
+    );
+    return product;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function getAllProducts(){
-    try {
-        const { rows } = await client.query(
-          `
+async function getAllProducts() {
+  try {
+    const { rows } = await client.query(
+      `
           SELECT *
           FROM products
         `
-        );
-        return rows;
-      } catch (error) {
-        console.error(error);
-      }
+    );
+    const productsWithPhotos = await attachPhotosToProducts(rows)
+
+    const finalProducts = await attachCategoriesToProducts(productsWithPhotos)
+ 
+    return finalProducts
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function getProductsById(id){
-    try {
-        const {
-          rows: [product],
-        } = await client.query(`
+async function getProductsById(id) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(`
         SELECT *   
         FROM products
         WHERE id=${id};
         `);
-        return product;
-      } catch (error) {
-        console.error(error);
-      }
+    return product;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function updateProduct({id, ...fields}){
-    const setString = Object.keys(fields)
+async function updateProduct({ id, ...fields }) {
+  const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
     .join(", ");
 
@@ -76,126 +80,222 @@ async function updateProduct({id, ...fields}){
   }
 }
 
-async function destroyProduct(id){
-try {
-    const {rows: [product]} = await client.query(
-        `
+async function destroyProduct(id) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
         DELETE FROM products
         WHERE id=$1
         RETURNING *;
       `,
-        [id]
-      );
-      return product
-} catch (error) {
+      [id]
+    );
+    return product;
+  } catch (error) {
     console.error(error);
-}
+  }
 }
 
-async function assignCategory({name, product_id}){
-    try {
-        const {
-            rows: [product]
-        } = await client.query(
-            `
+async function assignCategory({ name, product_id }) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
             INSERT into categories(name, product_id)
             VALUES($1, $2)
             RETURNING *;
             `,
-            [name, product_id]
-        );
-        return product
-    } catch (error) {
-        console.error(error)
-    }
+      [name, product_id]
+    );
+    return product;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function createInitialCategories() {
-    console.log('Starting to create categories...');
-    try {
-      const categoriesToCreate = [
-        { name: 'Consoles', product_id: 1 },
-        { name: 'Software', product_id: 2 },
-        { name: 'Hardware', product_id: 3  },
-      ];
-      const categories = await Promise.all(categoriesToCreate.map(assignCategory));
-  
-      console.log('Categories created:');
-      console.log(categories);
-      console.log('Finished creating categories!');
-    } catch (error) {
-      console.error('Error creating categories!');
-      throw error;
-    }
-  }
+  console.log("Starting to create categories...");
+  try {
+    const categoriesToCreate = [
+      { name: "Consoles", product_id: 1 },
+      { name: "Software", product_id: 1 },
+      { name: "Software", product_id: 2 },
+      { name: "Hardware", product_id: 3 },
+    ];
+    const categories = await Promise.all(
+      categoriesToCreate.map(assignCategory)
+    );
 
-async function getAllCategories(){
-    try {
-        const { rows } = await client.query(
-          `
+    console.log("Categories created:");
+    console.log(categories);
+    console.log("Finished creating categories!");
+  } catch (error) {
+    console.error("Error creating categories!");
+    throw error;
+  }
+}
+
+async function getAllCategories() {
+  try {
+    const { rows } = await client.query(
+      `
           SELECT DISTINCT name
           FROM categories
         `
-        );
-        console.log("These are all the categories: ", rows)
-        return rows;
-      } catch (error) {
-        console.error(error);
-      }
+    );
+    console.log("These are all the categories: ", rows);
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function attachPhotoToProduct({product_id, url, priority}){
-    try {
-        const {
-            rows: [product]
-        } = await client.query(
-            `
+async function attachPhotoToProduct({ product_id, url, priority }) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
             INSERT into product_photos(product_id, url, priority)
             VALUES($1, $2, $3)
             RETURNING *;
             `,
-            [product_id, url, priority]
-        );
-        return product
-    } catch (error) {
-        console.error(error)
-    }
+      [product_id, url, priority]
+    );
+    return product;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function createInitialPhotos() {
-    console.log('Starting to create photos...');
-    try {
-      const photosToCreate = [
-        { product_id: 1 ,url:"https://upload.wikimedia.org/wikipedia/commons/6/60/Disk_II.jpg" ,priority: 1},
-        { product_id: 1 ,url:"http://oldcomputers.net/pics/ti994-monitor.jpg" ,priority: 2},
-        { product_id: 2, url:"http://oldcomputers.net/pics/ti994-left.jpg" ,priority: 1 },
-        { product_id: 3, url:"https://upload.wikimedia.org/wikipedia/commons/8/8d/Epson-hx-20.jpg" ,priority: 1},
-      ];
-      const photos = await Promise.all(photosToCreate.map(attachPhotoToProduct));
-  
-      console.log('Photos created:');
-      console.log(photos);
-      console.log('Finished creating photos!');
-    } catch (error) {
-      console.error('Error creating photos!');
-      throw error;
-    }
-  }
+  console.log("Starting to create photos...");
+  try {
+    const photosToCreate = [
+      {
+        product_id: 1,
+        url: "https://upload.wikimedia.org/wikipedia/commons/6/60/Disk_II.jpg",
+        priority: 1,
+      },
+      {
+        product_id: 1,
+        url: "http://oldcomputers.net/pics/ti994-monitor.jpg",
+        priority: 2,
+      },
+      {
+        product_id: 2,
+        url: "http://oldcomputers.net/pics/ti994-left.jpg",
+        priority: 1,
+      },
+      {
+        product_id: 3,
+        url: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Epson-hx-20.jpg",
+        priority: 1,
+      },
+    ];
+    const photos = await Promise.all(photosToCreate.map(attachPhotoToProduct));
 
-async function getPhotosByProductId(product_id){
-    try {
-        const {
-          rows
-        } = await client.query(`
+    console.log("Photos created:");
+    console.log(photos);
+    console.log("Finished creating photos!");
+  } catch (error) {
+    console.error("Error creating photos!");
+    throw error;
+  }
+}
+
+async function getPhotosByProductId(product_id) {
+  try {
+    const { rows } = await client.query(`
         SELECT *    
         FROM product_photos
         WHERE product_id=${product_id};
         `);
-        console.log("These are the photos: ", rows)
-        return rows;
-      } catch (error) {
-        console.error(error);
-      }
+    console.log("These are the photos: ", rows);
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-module.exports = {createProduct, getAllProducts, getProductsById, updateProduct, destroyProduct, createInitialCategories, getAllCategories, createInitialPhotos, getPhotosByProductId}
+async function attachPhotosToProducts(products) {
+  // no side effects
+  const productsToReturn = [...products];
+  const binds = products.map((_, index) => `$${index + 1}`).join(", ");
+  const productIds = products.map((product) => product.id);
+  if (!productIds?.length) return [];
+  try {
+
+    const { rows: products } = await client.query(
+      `
+          SELECT product_photos.*
+          FROM products 
+          JOIN product_photos ON product_photos.product_id = products.id
+          WHERE product_photos.product_id IN (${binds});
+        `,
+      productIds
+    );
+
+
+    for (const product of productsToReturn) {
+
+      const photosToAdd = products.filter(
+        (product) => product.product_id === product.id
+      );
+
+      product.photos = photosToAdd;
+    }
+    return productsToReturn;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+async function attachCategoriesToProducts(products) {
+
+  const productsToReturn = [...products];
+  const binds = products.map((_, index) => `$${index + 1}`).join(", ");
+  const productIds = products.map((product) => product.id);
+  if (!productIds?.length) return [];
+  try {
+
+    const { rows: categories } = await client.query(
+      `
+          SELECT categories.*
+          FROM products 
+          JOIN categories ON categories.product_id = products.id
+          WHERE categories.product_id IN (${binds});
+        `,
+      productIds
+    );
+
+
+    for (const product of productsToReturn) {
+
+      const categoriesToAdd = categories.filter(
+        (category) => category.product_id === product.id
+      );
+
+      product.categories = categoriesToAdd;
+    }
+    return productsToReturn;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductsById,
+  updateProduct,
+  destroyProduct,
+  createInitialCategories,
+  getAllCategories,
+  createInitialPhotos,
+  getPhotosByProductId,
+};
