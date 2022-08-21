aa06cc9d87cd37330adb1e9a4b2ca20c03da6685
Eric Loving <ericdloving@gmail.com>
2022-08-21 10:40:11 -0500

    testing requireUser on orders post endpoint

--- api/orders.js
+++ api/orders.js
@@ -31,6 +31,7 @@ router.post("/", requireUser, async (req, res, next) => {
   const { cart_id, address_id, status } = req.body;
 
   try {
+    console.log(cart_id, address_id, status)
     convertCartToPurchased({cart_id})
     const newOrder = await createOrder({
       cart_id,
